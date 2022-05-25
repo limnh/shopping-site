2bf1467a4dea1992bc99e932bc18e3a21207f1f5
Nicole Lim <limnhpdx@gmail.com>
2022-05-25 12:59:43 -0700

    Continue shoppingsite.py tests

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -65,9 +65,14 @@ def show_shopping_cart():
     # The logic here will be something like:
     #
     # - get the cart dictionary from the session
+    cart = session.get("cart", {})
     # - create a list to hold melon objects and a variable to hold the total
     #   cost of the order
+    melon_list = []
+    order_cost = 0
     # - loop over the cart dictionary, and for each melon id:
+    # for melon in :
+
     #    - get the corresponding Melon object
     #    - compute the total cost for that type of melon
     #    - add this to the order total
@@ -90,11 +95,23 @@ def add_to_cart(melon_id):
     cart'."""
 
     # TODO: Finish shopping cart functionality
+    
+    if "cart" in session:
+        cart = session["cart"]
+    else:
+        cart = session["cart"] = {}
+    print(cart)
+    
+    # cart[melon_id] = cart.get(melon_id, 0) + 1    
+    
+    if cart[melon_id] in cart:
+        return
+    else:
+        cart[melon_id] = cart.get(melon_id)
+    print(cart)
+    
+    return redirect("/cart")
 
-    # The logic here should be something like:
-    #
-    # - check if a "cart" exists in the session, and create one (an empty
-    #   dictionary keyed to the string "cart") if not
     # - check if the desired melon id is the cart, and if not, put it in
     # - increment the count for that melon id by 1
     # - flash a success message
